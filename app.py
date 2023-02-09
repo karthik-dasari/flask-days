66339303ac840a2804d5ca8eea2560090c2399b1
karthik-dasari <karthik.dasari2001@gmail.com>
2023-02-09 19:29:09 +0530

    first commit

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from mysql.connector import connect
 
 con=connect(host='localhost'
             ,port=3306
             ,database='cse'
             ,user='root')
-
-cur=con.cursor()
-cur.execute("insert into data values('Rahul','1' )")
-con.commit()
+# cur=con.cursor()
+# cur.execute("SELECT * FROM data where roll=%s",(roll))
+# res=cur.fetchall()
 
 abc = Flask(__name__)
+abc.secret_key="hfuegik"
 
 @abc.route("/")
 @abc.route("/index")
 @abc.route("/home")
 def hello():
-    return render_template("home.html")
+    session['user']='karthik'
+    return redirect('myform')
 
 @abc.route("/about")
 def about():
+    session.clear()
     return "About Page"
 
 @abc.route("/myform", methods=["GET", "POST"])
 def myform():
-    if request.method == "POST":
-        user=request.form["username"]
-        password=request.form["password"]
-        return render_template("myform.html", user=user)
+    if session.get('name'):
+        return render_template("myform.html", user=session["name"])
     else:
-        return render_template("myform.html")
+        if request.method == "POST":
+            name=request.form["name"]
+            roll=request.form["roll"]
+            cur=con.cursor()
+            cur.execute("insert into data values(%s,%s)",(name,roll))
+            con.commit()
+            session["name"]=name
+            return render_template("myform.html", user=name)
+        else:
+            return render_template("myform.html")
 
 @abc.route('/name/<string:s>')
 def name(s):
